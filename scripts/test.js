4e21b2b6e0b5a64c420cfbea3aabb66d0d2d6434
thiago willaredt <thiagowillaredt@MacBook-Pro-de-thiago.local>
2024-09-04 01:34:54 -0300

    adicionando5  actions k6

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
     vus: 100,
@@ -21,4 +21,11 @@ check(res, {
 
 })
     
+}
+
+
+export function handleSummary(data){
+    return{
+        "index.html": htmlReport(data),
+    };
 }
